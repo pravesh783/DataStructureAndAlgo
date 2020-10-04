6c76d010220fb0a6b71b8edd4ac2d59519f6ba3a
Prince Vishwakarma <43395338+princev89@users.noreply.github.com>
2020-10-04 20:49:08 +0530

    Create knapsack.cpp

---
+++ dynamic_programming/knapsack.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int wt[] = {1,3,4,5};
+    int val[] = {1,4,5,7};
+    int n = 4;
+    int w = 7;
+    int t[5][8];
+    
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < w+1;  j++ ){
+            if(i == 0 || j == 0)
+              t[i][j] = 0;
+              else t[i][j] = -1;
+        }
+    }
+    
+    // for(int i = 0; i < n+1; i++){
+    //     for(int j = 0; j < w+1;  j++ ){
+    //         cout << t[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
+    
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < w+1; j++){
+            if(wt[i-1] <= j){
+                //we can include element or not 
+                t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+            }
+            else{
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    cout << "Matrix(TopDown approach) will look like\n";
+     for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < w+1;  j++ ){
+            cout << t[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    cout << t[n][w];
+    return 0;
+}
